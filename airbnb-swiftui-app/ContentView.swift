b40ffdb7af394730e63414ea59c136fca3456d55
Heri Susanto <herisusanto.app@gmail.com>
2024-01-17 00:03:03 +0700

    Initial Commit

---
+++ airbnb-swiftui-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  airbnb-swiftui-app
+//
+//  Created by loratech on 17/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ airbnb-swiftui-app/airbnb_swiftui_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnb_swiftui_appApp.swift
+//  airbnb-swiftui-app
+//
+//  Created by loratech on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct airbnb_swiftui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
