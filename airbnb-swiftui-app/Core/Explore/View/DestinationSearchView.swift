2dd494666928cc4d7dac8af5ca302490683c0520
Heri Susanto <herisusanto.app@gmail.com>
2024-01-17 21:33:14 +0700

    Add Destination Search View Create Collapsible Destination View Modifier

---
+++ airbnb-swiftui-app/Core/Explore/View/DestinationSearchView.swift
@@ -0,0 +1,175 @@
+//
+//  DestinationSearchView.swift
+//  airbnb-swiftui-app
+//
+//  Created by loratech on 17/01/24.
+//
+
+import SwiftUI
+
+enum DestinationSearchOption {
+    case location
+    case dates
+    case guests
+}
+
+struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @State private var destination = ""
+    @State private var selectedOption: DestinationSearchOption = .location
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var numGuests = 0
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Button{
+                    withAnimation(.snappy) {
+                        show.toggle()
+                    }
+                }label: {
+                    Image(systemName: "xmark.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
+                }
+                
+                Spacer()
+                
+                if !destination.isEmpty {
+                    Button("Clear") {
+                        destination = ""
+                    }
+                    .foregroundStyle(.black)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
+                
+            }
+            .padding()
+            
+            VStack(alignment: .leading) {
+                if selectedOption == .location {
+                    Text("Where to?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("Search destination",text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                } else {
+                    CollapsePickerView(title: "Where", description: "Add destination")
+                }
+                
+            }
+            .modifier(CollapsibleDestinationViewModifier())
+            .frame(height: selectedOption == .location ? 180 : 64)
+            .onTapGesture {
+                withAnimation(.snappy) {
+                    selectedOption = .location
+                }
+            }
+            
+            VStack(alignment: .leading) {
+                if selectedOption == .dates {
+                    Text("When's your trip?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    VStack {
+                        DatePicker("From", selection: $startDate, displayedComponents: .date)
+                        
+                        Divider()
+                        
+                        DatePicker("To", selection: $endDate, displayedComponents: .date)
+                    }
+                    .foregroundStyle(.gray)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
+                } else {
+                    CollapsePickerView(title: "When", description: "Add dates")
+                }
+                
+            }
+            .modifier(CollapsibleDestinationViewModifier())
+            .frame(height: selectedOption == .dates ? 180 : 64)
+            .onTapGesture {
+                withAnimation(.snappy) {
+                    selectedOption = .dates
+                }
+            }
+            
+            VStack(alignment: .leading) {
+                if selectedOption == .guests {
+                    Text("Who's coming?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Stepper {
+                        Text("\(numGuests) Adults")
+                    } onIncrement: {
+                        guard numGuests < 5 else { return }
+                        numGuests += 1
+                    } onDecrement: {
+                        guard numGuests > 0 else { return }
+                        numGuests -= 1
+                    }
+                    
+                } else {
+                    CollapsePickerView(title: "Who", description: "Add guests")
+                }
+            }
+            .modifier(CollapsibleDestinationViewModifier())
+            .frame(height: selectedOption == .guests ? 120 : 64)
+            .onTapGesture {
+                withAnimation(.snappy) {
+                    selectedOption = .guests
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    DestinationSearchView(show: .constant(false))
+}
+
+struct CollapsibleDestinationViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+        
+    }
+}
+
+struct CollapsePickerView: View {
+    var title: String
+    var description: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                Spacer()
+                Text(description)
+            }
+            .fontWeight(.semibold)
+            .font(.subheadline)
+        }
+    }
+}
--- airbnb-swiftui-app/Core/Explore/View/ExploreView.swift
+++ airbnb-swiftui-app/Core/Explore/View/ExploreView.swift
@@ -7,26 +7,39 @@
 
 import SwiftUI
 
+
+
 struct ExploreView: View {
+    @State private var showDestinationSearchView = false
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                SearchAndFilterBar()
-                ScrollView {
-                    LazyVStack(spacing: 32) {
-                        ForEach(0...20, id: \.self){ listing in
-                            NavigationLink(value: listing){
-                                ListingItemView()
-                                    .frame(height: 400)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            if showDestinationSearchView {
+                DestinationSearchView(show: $showDestinationSearchView)
+            } else {
+                VStack {
+                    SearchAndFilterBar()
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                showDestinationSearchView.toggle()
                             }
                         }
+                    ScrollView {
+                        LazyVStack(spacing: 32) {
+                            ForEach(0...20, id: \.self){ listing in
+                                NavigationLink(value: listing){
+                                    ListingItemView()
+                                        .frame(height: 400)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                }
+                            }
+                        }
+                        .padding()
                     }
-                    .padding()
-                }
-                .navigationDestination(for: Int.self) { listing in
-                    ListingDetailView()
-                        .navigationBarBackButtonHidden()
+                    .navigationDestination(for: Int.self) { listing in
+                        ListingDetailView()
+                            .navigationBarBackButtonHidden()
+                    }
                 }
             }
         }
